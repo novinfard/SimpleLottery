e800b24947c1eca5306997728f5ce5698ff950a3
Soheil Novinfard <soheil.novinfard@pulselive.com>
2021-02-23 17:52:27 +0000

    Initial Commit

---
+++ Shared/SimpleLotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLotteryApp.swift
+//  Shared
+//
+//  Created by Soheil  Novinfard on 23/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
