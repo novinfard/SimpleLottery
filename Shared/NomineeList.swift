fe7d443a616ff5731e136e584862db81c27b74cf
Soheil Novinfard <soheil.novinfard@pulselive.com>
2021-02-23 20:24:45 +0000

    Add list of nominees

---
+++ Shared/NomineeList.swift
@@ -0,0 +1,18 @@
+//
+//  NomineeList.swift
+//  SimpleLottery
+//
+//  Created by Soheil  Novinfard on 23/02/2021.
+//
+
+import Foundation
+
+extension Nominee {
+    static let listOfNominee: [Nominee] = [
+        .init(name: "Mr. Hashemi", user: "Hashaaa", blank: true),
+        .init(name: "Jack", user: "jackJack22"),
+        .init(name: "Mack", user: "mackMack13"),
+        .init(name: "Pack", user: "packPackDom"),
+        .init(name: "Mohsen", user: "AghaMohsen", blank: true)
+    ]
+}
