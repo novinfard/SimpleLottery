056904b501644b65b340251d60be0d1f00762a18
Soheil Novinfard <soheil.novinfard@pulselive.com>
2021-02-23 20:26:51 +0000

    Update main content view to reflect the election publisher changes and add the result UI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var electionPublisher = ElectionPublisher(nomineeList: Nominee.listOfNominee)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🏆 The winner is:")
+                .font(.headline)
+            Spacer()
+                .frame(height: 20)
+            Text(descriptionText)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding(15)
+            Text(electionPublisher.winner.name)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(textColor)
+            Text("@" + electionPublisher.winner.user)
+                .foregroundColor(textColor)
+            Image(resultImageName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 200)
+
+        }
+    }
+
+    var textColor: Color {
+        guard self.electionPublisher.electionDone else { return .black }
+        return self.electionPublisher.winner.color
+    }
+
+    var resultImageName: String {
+        guard self.electionPublisher.electionDone else { return "" }
+        return self.electionPublisher.winner.resultImageName
+    }
+
+    var descriptionText: String {
+        guard electionPublisher.electionDone else { return "" }
+        return electionPublisher.winner.resultText
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+fileprivate extension Nominee {
+    var color: Color {
+        blank == true ? .red : .orange
+    }
+
+    var resultText: String {
+        if blank == true {
+            return "😢 It was blank! \r\n The election has no winner!"
+        } else {
+            return "🎉🤑 Congrats! \r\n You're the winner "
+        }
+    }
+
+    var resultImageName: String {
+        blank == true ? "oops" : "happy"
     }
 }
