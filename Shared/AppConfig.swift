b5fe4296d4eb2dae57b0c19603417cda5d7ee79d
Soheil Novinfard <novinfard@gmail.com>
2021-07-28 01:25:01 +0100

    Add application config settings

---
+++ Shared/AppConfig.swift
@@ -0,0 +1,13 @@
+//
+//  AppConfig.swift
+//  SimpleLottery (iOS)
+//
+//  Created by Soheil  Novinfard on 27/07/2021.
+//
+
+import Foundation
+
+struct AppConfig {
+    static let userListEndPoint = "http://novinfard.com/API/SimpleLottery/users.json"
+    static let lotteryListEndPoint = "http://novinfard.com/API/SimpleLottery/usersInLottery.json"
+}
