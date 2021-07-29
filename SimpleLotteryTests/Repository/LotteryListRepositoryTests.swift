611cbad873b0941bbe7111503204f38fc68180eb
Soheil Novinfard <novinfard@gmail.com>
2021-07-29 02:30:13 +0100

    Add unit tests for `LotteryListRepository`

---
+++ SimpleLotteryTests/Repository/LotteryListRepositoryTests.swift
@@ -0,0 +1,84 @@
+//
+//  LotteryListRepositoryTests.swift
+//  SimpleLotteryTests
+//
+//  Created by Soheil  Novinfard on 28/07/2021.
+//
+
+import XCTest
+import Combine
+@testable import SimpleLottery
+
+class LotteryListRepositoryTests: XCTestCase {
+
+    private var subscriptions = Set<AnyCancellable>()
+
+    override func tearDown() {
+        subscriptions = []
+    }
+
+    func test_whenUrlIsValid_returnsSuccessfully() {
+        let sut = createSUT()
+        var receivedUsers = [LotteryUser]()
+
+        let expectation = expectation(description: "Expect to get a list of users")
+
+        sut.modelPublisher
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    XCTFail("Failed to return with error \(error)")
+                }
+                expectation.fulfill()
+            }, receiveValue: {
+                receivedUsers = $0
+            })
+            .store(in: &subscriptions)
+
+        sut.load()
+
+        wait(for: [expectation], timeout: 10)
+
+        XCTAssertTrue(!receivedUsers.isEmpty, "The list of returned users should not be empty")
+    }
+
+    func test_whenUrlIsValid_parsesUserCorrectly() {
+        let sut = createSUT()
+        var receivedUsers = [LotteryUser]()
+
+        let expectation = expectation(description: "Expect to get a list of users parsed correctly")
+
+        sut.modelPublisher
+            .sink(receiveCompletion: { completion in
+                expectation.fulfill()
+            }, receiveValue: {
+                receivedUsers = $0
+            })
+            .store(in: &subscriptions)
+
+        sut.load()
+
+        wait(for: [expectation], timeout: 10)
+
+        let user = try? XCTUnwrap(
+            receivedUsers.first(where: {$0.userId == 182 }),
+            "The user with id `182` should be found in the list"
+        )
+
+        XCTAssertEqual(user?.void, true)
+    }
+
+    private func createSUT() -> LotteryListRepository {
+        let session = URLSession.shared
+        session.configuration.requestCachePolicy = .reloadIgnoringCacheData
+        return LotteryListRepositoryImplementation(
+            session: session,
+            endpoint: LotteryListRepositoryImplementation.mockUrl()
+        )
+    }
+}
+
+extension LotteryListRepositoryImplementation {
+    static func mockUrl() -> URL? {
+        return Bundle(for: LotteryListRepositoryTests.self).url(forResource: "users", withExtension: "json")
+    }
+}
